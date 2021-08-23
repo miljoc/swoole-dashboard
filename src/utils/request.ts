9f2b71b04916b64ee56ca79e070b2052ee50f9bf
codinghuang <codinghuang@qq.com>
2021-08-23 15:34:52 +0800

    Optimize request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -24,22 +24,14 @@ service.interceptors.request.use(
 // Response interceptors
 service.interceptors.response.use(
   (response) => {
-    // Some example codes here:
-    // code == 20000: success
-    // code == 50001: invalid access token
-    // code == 50002: already login in other place
-    // code == 50003: access token expired
-    // code == 50004: invalid user (user not exist)
-    // code == 50005: username or password is incorrect
-    // You can change this part for your own usage.
     const res = response.data
     if (res.code !== 0) {
       Message({
-        message: res.message || 'Error',
+        message: res.data || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
-      return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(res.data || 'Error'))
     } else {
       return response.data
     }

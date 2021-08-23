b02a33539cdd2cfdfce6b8ba90f615813da677b3
codinghuang <codinghuang@qq.com>
2021-08-23 11:24:30 +0800

    Refactor utils.request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -33,26 +33,12 @@ service.interceptors.response.use(
     // code == 50005: username or password is incorrect
     // You can change this part for your own usage.
     const res = response.data
-    if (res.code !== 20000) {
+    if (res.code !== 0) {
       Message({
         message: res.message || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        MessageBox.confirm(
-          'You have been logged out, try to login again.',
-          'Log out',
-          {
-            confirmButtonText: 'Relogin',
-            cancelButtonText: 'Cancel',
-            type: 'warning'
-          }
-        ).then(() => {
-          UserModule.ResetToken()
-          location.reload() // To prevent bugs from vue-router
-        })
-      }
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
       return response.data

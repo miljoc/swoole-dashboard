b954c959ebb38db89bcd25b822e8256dbc1997fb
codinghuang <codinghuang@qq.com>
2021-08-27 11:54:45 +0800

    Format

--- src/api/workers.ts
+++ src/api/workers.ts
@@ -3,17 +3,17 @@ import request from '@/utils/request'
 export const getWorkerInfo = (id: number) =>
   request({
     url: `/api/getpid/worker-${id}`,
-    method: 'get',
-})
+    method: 'get'
+  })
 
 export const getCoroutineStats = (id: number) =>
   request({
     url: `/api/coroutine_stats/worker-${id}`,
-    method: 'get',
-})
+    method: 'get'
+  })
 
 export const getWorkerMemoryUsage = (id: number) =>
   request({
     url: `/api/memory_usage/worker-${id}`,
-    method: 'get',
-})
+    method: 'get'
+  })

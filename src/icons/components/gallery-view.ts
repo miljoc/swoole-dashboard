8042145b7ee727cd64f68da99bf6eb93696c8638
matyhtf <mikan.tenny@gmail.com>
2021-09-15 15:27:26 +0800

    更新Icon [2]

---
+++ src/icons/components/gallery-view.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'gallery-view': {
+    width: 200,
+    height: 200,
+    viewBox: '0 0 1024 1024',
+    data: '<defs/><path pid="0" d="M288 287.872V64H64v224l224-.128zm112 672h224V736H400v224zm-336 0h224V736H64v224zm0-336h224v-224H64v224zm336 0h224v-224H400v224zm336-336h224V64H736v223.872zm-112 0V64H400v224l224-.128zm336 336v-224H736v224h224zm0 336V736H736v224h224z" _fill="#262626"/>'
+  }
+})

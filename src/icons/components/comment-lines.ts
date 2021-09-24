dfdd1e254ab2aa8c19ea84447357248b2588c230
matyhtf <mikan.tenny@gmail.com>
2021-09-24 20:02:33 +0800

    添加 icon

---
+++ src/icons/components/comment-lines.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'comment-lines': {
+    width: 200,
+    height: 200,
+    viewBox: '0 0 1024 1024',
+    data: '<defs/><path pid="0" d="M64 64h896v709.312H363.648L64 960V64zm278.336 634.688h542.976v-560H138.688v686.848l203.648-126.848zM288 288h448v112H288V288zm0 186.688h448v112H288v-112z" _fill="#262626"/>'
+  }
+})

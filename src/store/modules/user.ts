62baf8171f1d51eda06bdc52243ef6da11bbc2bd
yun <52o@qq52o.me>
2021-10-08 17:32:32 +0000

    Update lang (#11)
    Co-authored-by: yunbaoi <52o@qq52o.me>
    Reviewed-on: https://git.swoole.com/swoole/swoole-dashboard/pulls/11
    Co-authored-by: yun <52o@qq52o.me>
    Co-committed-by: yun <52o@qq52o.me>

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,6 +1,6 @@
 import { VuexModule, Module, Action, Mutation, getModule } from 'vuex-module-decorators'
 import { login, logout, getUserInfo } from '@/api/users'
-import { getToken, setToken, removeToken } from '@/utils/cookies'
+import { getToken, setToken, removeToken, getLanguage } from '@/utils/cookies'
 import store from '@/store'
 
 export interface IUserState {
@@ -43,6 +43,7 @@ class User extends VuexModule implements IUserState {
     const formData = new FormData()
     formData.append('name', username.trim())
     formData.append('password', password)
+    formData.append('lang', getLanguage() as string)
     const { data } = await login(formData)
     setToken(data.accessToken)
 

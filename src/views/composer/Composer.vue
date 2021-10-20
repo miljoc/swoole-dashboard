5537d62626705c4e976b7e8ac4da0a5a30bea404
yun <52o@qq52o.me>
2021-10-20 23:20:37 +0000

    Update composer info (#18)
    Co-authored-by: yunbaoi <52o@qq52o.me>
    Co-authored-by: lufei <lufei@swoole.com>
    Co-authored-by: sy-records <52o@qq52o.cn>
    Reviewed-on: https://git.swoole.com/swoole/swoole-dashboard/pulls/18
    Co-authored-by: yun <52o@qq52o.me>
    Co-committed-by: yun <52o@qq52o.me>

--- src/views/composer/Composer.vue
+++ src/views/composer/Composer.vue
@@ -11,6 +11,12 @@
           :value="item.value">
         </el-option>
       </el-select>
+
+      <el-input v-model="input" id="search" style="margin: 0 10px 10px 0;width: 300px;" @keyup.enter.native="searchFilter" :placeholder="$t('composer.name')"></el-input>
+
+      <el-button type="primary" @click="searchFilter" icon="el-icon-search">{{ $t('common.search') }}</el-button>
+
+      <el-button type="default" style="color:#909399;" @click="clearFilter"><svg-icon name="clean" /> {{ $t('common.clear') }}</el-button>
     </el-row>
 
     <el-row>
@@ -23,7 +29,6 @@
           <span v-else @click="mouseOver(root.aliases)"  style="color:#1890ff;">{{ root.aliases }}</span>
         </el-descriptions-item>
         <el-descriptions-item v-if="showDescriptions" :label="$t('composer.type')">{{ root.type }}</el-descriptions-item>
-        <el-descriptions-item v-if="showDescriptions" :label="$t('composer.reference')">{{ root.reference }}</el-descriptions-item>
         <el-descriptions-item :label="$t('composer.installPath')">{{ root.install_path }}</el-descriptions-item>
       </el-descriptions>
     </el-row>
@@ -46,17 +51,17 @@
           {{ scope.row.name }}
         </template>
       </el-table-column>
-      <el-table-column :label="$t('composer.version')" width="100">
+      <el-table-column :label="$t('composer.version')" width="250">
         <template slot-scope="scope">
           {{ scope.row.pretty_version }}
         </template>
       </el-table-column>
-      <el-table-column :label="$t('composer.type')" width="200">
+      <el-table-column :label="$t('composer.type')" width="250">
         <template slot-scope="scope">
           {{ scope.row.type }}
         </template>
       </el-table-column>
-      <el-table-column :label="$t('composer.aliases')" width="100">
+      <el-table-column :label="$t('composer.aliases')" width="150">
         <template slot-scope="scope"  >
           <span  v-if="scope.row.is_obg === '-'" >{{ scope.row.is_obg }}</span>
           <div v-else @click="mouseOver(scope.row)" >
@@ -69,11 +74,6 @@
           {{ scope.row.dev_requirement }}
         </template>
       </el-table-column>
-      <el-table-column :label="$t('composer.reference')" width="400" >
-        <template slot-scope="scope" >
-          {{ scope.row.reference }}
-        </template>
-      </el-table-column>
       <el-table-column
         align="center"
         :label="$t('common.actions')"
@@ -81,13 +81,20 @@
         style="text-overflow:ellipsis;overflow:hidden;"
       >
         <template slot-scope="{row}">
-          <el-button type="info" size="mini" :disabled="/^__root__/.test(row.name)" @click="dialogVisibleDiv(row)" style="float: left;">
+            <el-button type="info" size="mini" :disabled="/^__root__/.test(row.name) || row.reference === '-'" @click="dialogVisibleDiv(row)" style="float: left;">
             <svg-icon name="detail" /> {{ $t('common.detail') }}
           </el-button>
         </template>
       </el-table-column>
 
     </el-table>
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getComposer"
+    />
 
     <el-dialog
       :title="$t('common.detail')"
@@ -129,8 +136,11 @@ import axios from 'axios'
   }
 })
 export default class extends Vue {
+  private input: any = ''
+  private packageName: any = ''
   private list: any = []
   private nowPackageData: any = []
+  private handleAllList: any = []
   private optionsValue = ''
   private options: any = []
   private value_content: any = ''
@@ -141,9 +151,38 @@ export default class extends Vue {
   private dialogVisible = false
   private dialogTableVisible = false
   private showDescriptions = true
+  private total = 0
+  private listQuery = {
+    page: 1,
+    limit: 10
+  }
+
+  private optionsName: any = []
+  private optionsNameValue = ''
+  private versions_data_list: any = []
 
   created() {
     this.getComposer()
+  }
+
+  // 搜索
+  private searchFilter(): void {
+    this.listQuery.page = 1
+    this.input = this.input.replace(/(^\s*)|(\s*$)/g, '')
+    this.setData(this.list[this.optionsValue])
+  }
+
+  // 清除
+  private clearFilter(): void {
+    this.input = ''
+    this.setData(this.list[this.optionsValue])
+  }
+
+  private handleComposerData(packageName: string) {
+    this.packageName = packageName
+    this.input = ''
+    this.listQuery.page = 1
+    this.setData(this.list[packageName])
   }
 
   private setData(packageData: any) {
@@ -161,7 +200,9 @@ export default class extends Vue {
     this.showDescriptions = !/^__root__/.test(this.root.name)
 
     const versions_data = []
+    const versions_data_name = []
     this.versions = packageData.versions
+
     for (const names in this.versions) {
       const id = index + 1
       let value: any = '-'
@@ -185,14 +226,84 @@ export default class extends Vue {
         type: this.versions[names].type ? this.versions[names].type : '-',
         version: this.versions[names].version ? this.versions[names].version : '-'
       })
+      versions_data_name.push(names.split('/')[0])
       index++
     }
 
-    this.nowPackageData = versions_data
+    this.versions_data_list = versions_data
+
+    const name_data = this.newArr(versions_data_name)
+
+    this.optionsName = []
+    for (let i = 0; i < name_data.length; i++) {
+      this.optionsName.push({ value: name_data[i] })
+    }
+
+    const input = this.input ? this.input : this.optionsNameValue
+
+    this.handleAllList = JSON.parse(JSON.stringify(versions_data))
+
+    if (input.length > 0) {
+      const arr:any = []
+      for (let i = 0; i < versions_data.length; i++) {
+        if (versions_data[i].name.indexOf(input) >= 0) {
+          arr.push(versions_data[i].name)
+        }
+      }
+
+      const tmpList:any = []
+      if (arr.length > 0) {
+        for (let i = 0; i < arr.length; i++) {
+          tmpList.push(this.handleAllList.filter((item: any) => {
+            let mark = true
+            if (item.name !== arr[i]) {
+              mark = false
+            }
+            return mark
+          })[0])
+        }
+
+        const list_s = []
+        for (let i = 0; i < tmpList.length; i++) {
+          list_s.push({
+            name: tmpList[i].name,
+            version: tmpList[i].version,
+            id: i + 1,
+            aliases: tmpList[i].aliases,
+            dev_requirement: tmpList[i].dev_requirement,
+            install_path: tmpList[i].install_path,
+            is_obg: tmpList[i].is_obg,
+            pretty_version: tmpList[i].pretty_version,
+            reference: tmpList[i].reference,
+            type: tmpList[i].type
+          })
+        }
+        this.versions_data_list = list_s
+      } else {
+        this.versions_data_list = []
+      }
+    }
+
+    const tmpData = []
+    for (const item of this.versions_data_list) {
+      if (item.id >= (this.listQuery.page - 1) * this.listQuery.limit && item.id < this.listQuery.page * this.listQuery.limit) {
+        tmpData.push(item)
+      }
+    }
+
+    this.nowPackageData = tmpData
+    this.total = this.versions_data_list.length
   }
 
-  private handleComposerData(packageName: string) {
-    this.setData(this.list[packageName])
+  private newArr(array: any) {
+    const arrs = []
+    for (let i = 0; i < array.length; i++) {
+      // 如果临时数组里没有当前数组的当前值，则把当前值push到新数组里面
+      if (arrs.indexOf(array[i]) === -1) {
+        arrs.push(array[i])
+      }
+    }
+    return arrs
   }
 
   async mouseOver(row: any) {
@@ -221,11 +332,12 @@ export default class extends Vue {
     const { data } = await getComposer()
 
     this.list = data
-
+    this.options = []
     for (const package_data in data) {
       this.options.push({ value: package_data })
     }
     this.optionsValue = this.options[0]?.value || ''
+    this.optionsValue = this.packageName.length > 0 ? this.packageName : this.optionsValue
     if (this.optionsValue) {
       this.setData(this.list[this.optionsValue])
     }

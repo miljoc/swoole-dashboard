c9b33349af3bdee7a1ca51a951d672044f8317ec
sy-records <52o@qq52o.cn>
2021-10-22 18:21:58 +0800

    Sync lang

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -2,11 +2,11 @@ export default {
   route: {
     'Swoole Dashboard': 'Home',
     'Worker Stats': 'Worker Stats',
+    ZendVM: 'ZendVM',
     Extensions: 'Extensions',
     'Extension Info': 'Extension Info',
     'Included Files': 'Included Files',
     'Included File Info': 'Included File Info',
-    ZendVM: 'ZendVM',
     Composer: 'Composer',
     'GC Status': 'GC Status',
     Classes: 'Classes',
@@ -135,11 +135,6 @@ export default {
   },
   components: {
     documentation: 'Documentation',
-    tinymceTips: 'Rich text is a core feature of the management backend, but at the same time it is a place with lots of pits. In the process of selecting rich texts, I also took a lot of detours. The common rich texts on the market have been basically used, and I finally chose Tinymce. See the more detailed rich text comparison and introduction.',
-    stickyTips: 'when the page is scrolled to the preset position will be sticky on the top.',
-    backToTopTips1: 'When the page is scrolled to the specified position, the Back to Top button appears in the lower right corner',
-    backToTopTips2: 'You can customize the style of the button, show / hide, height of appearance, height of the return. If you need a text prompt, you can use element-ui el-tooltip elements externally',
-    imageUploadTips: 'Since I was using only the vue@1 version, and it is not compatible with mockjs at the moment, I modified it myself, and if you are going to use it, it is better to use official version.',
     changeLanguageTips: 'Switch Language Success',
     changeAdminServerTips: 'Switch admin-server：',
     deleteAdminServerTips: 'Delete admin-server：'
@@ -246,7 +241,7 @@ export default {
     constantName: 'Constant Name',
     constantValue: 'Constant Value',
     properties: 'Properties',
-    propertiesModifiers: 'Modifiers',
+    staticProperties: 'Static Properties',
     modifiers: 'Modifiers',
     propertiesName: 'Property Name',
     propertiesValue: 'Property Value',
